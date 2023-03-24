522a574c1607e057e9b5978301e8e033401551a4
josephleung5 <joseph.leung5@gmail.com>
2023-03-24 17:11:07 +0000

    feat: install react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import './App.scss';
-import Dashboard from './components/Dashboard/Dashboard';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Dashboard from './components/Dashboard/Dashboard'
+import Login from './components/Login/Login'
+import './App.scss'
 
-function App() {
-  return (
-    <>
-      <Dashboard />
-    </>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<div>homepage</div>}/>
+          <Route path="/dashboard" element={<Dashboard />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="*" element={<div>Something is not working...</div>}/>
+        </Routes>
+      </Router>
+    );
+  }
 }
 
-export default App;
+export default App
